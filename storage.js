fb8b5b57c54a1395332641f1646cd1e9aa43d6c0
princiya <princiya.marina@gmail.com>
2017-05-17 19:14:15 +0200

    basic storage class layout. closes #6

---
+++ storage.js
@@ -0,0 +1,21 @@
+class storage {
+	constructor() {
+		return this.store;
+	}
+
+	create() {
+		//add to the store object
+	}
+
+	read() {
+		//read from the store	
+	}
+
+	update() {
+		//update the store
+	}
+
+	remove() {
+		//delete from the store
+	}
+}
