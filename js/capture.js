f58f741693540c0d9f233f7b89f02d199721983c
Bianca Danforth <biancadanforth@gmail.com>
2017-05-25 14:14:35 -0700

    Add new ESLint rules. Fixes #29.
    Also included eslint-plugin-no-unsanitized (https://github.com/mozilla/eslint-plugin-no-unsanitized).

--- js/capture.js
+++ js/capture.js
@@ -5,8 +5,9 @@ const capture = {
 
   addListeners() {
     // listen for each HTTP response
-    browser.webRequest.onResponseStarted.addListener(this.sendThirdParty, {urls: ['<all_urls>']}
-    );
+    browser.webRequest.onResponseStarted.addListener(
+      this.sendThirdParty,
+      {urls: ['<all_urls>']});
     // listen for tab updates
     browser.tabs.onUpdated.addListener(this.sendFirstParty);
   },
@@ -35,12 +36,10 @@ const capture = {
     // ignore about:* pages and non-visible tabs
     if (tab.status === 'complete'
       && tabUrl.protocol !== 'about:'
-      && tabId !== browser.tabs.TAB_ID_NONE)
-       {
-      const firstPartyData = {
-        faviconUrl: tab.favIconUrl,
-      };
-      console.log('storage.firstPartyRequest:', tabUrl.hostname, firstPartyData);
+      && tabId !== browser.tabs.TAB_ID_NONE) {
+      const firstPartyData = { faviconUrl: tab.favIconUrl };
+      console.log('storage.firstPartyRequest:',
+        tabUrl.hostname, firstPartyData);
     }
   }
 };
