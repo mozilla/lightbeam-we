ea176dd3c0747482387797e8a115338b55d7c2b5
princiya <princiya.marina@gmail.com>
2017-05-25 12:57:05 +0200

    files moved into js dir. fixes #25

---
+++ js/capture.js
@@ -0,0 +1,46 @@
+const capture = {
+  init() {
+    this.addListeners();
+  },
+
+  addListeners() {
+    // listen for each HTTP response
+    browser.webRequest.onResponseStarted.addListener(this.sendThirdParty, {urls: ['<all_urls>']}
+    );
+    // listen for tab updates
+    browser.tabs.onUpdated.addListener(this.sendFirstParty);
+  },
+
+  // capture third party requests
+  async sendThirdParty(response) {
+    const tab = await browser.tabs.get(response.tabId);
+    let tabUrl = new URL(tab.url);
+    let targetUrl = new URL(response.url);
+    let originUrl = new URL(response.originUrl);
+
+    if (targetUrl !== tabUrl) {
+      const thirdPartyData = {
+        document: tabUrl.hostname,
+        target: targetUrl.hostname,
+        origin: originUrl.hostname,
+        requestTime: response.timeStamp
+      };
+      console.log('storage.thirdPartyRequest:', tabUrl, thirdPartyData);
+    }
+  },
+
+  // capture first party requests
+  sendFirstParty(tabId, changeInfo, tab) {
+    const tabUrl = new URL(tab.url);
+    // ignore about:* pages and non-visible tabs
+    if (tab.status === 'complete'
+      && tabUrl.protocol !== 'about:'
+      && tabId !== browser.tabs.TAB_ID_NONE)
+       {
+      const firstPartyData = {
+        faviconUrl: tab.favIconUrl,
+      };
+      console.log('storage.firstPartyRequest:', tabUrl.hostname, firstPartyData);
+    }
+  }
+};
