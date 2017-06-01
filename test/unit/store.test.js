325484163bdba1bcad33b2432a69d3c4f789e438
Princiya <princiya.marina@gmail.com>
2017-06-01 14:20:41 +0200

    chai assert used. fixes #33

--- test/unit/store.test.js
+++ test/unit/store.test.js
@@ -2,8 +2,8 @@
 /* eslint no-console: "off" */
 
 /*
-* @todo resolve 'expect' is undefined and turn 'on' the above eslint rules
-* @todo use eslint-plugin-chai to resolve 'expect'
+* @todo resolve 'assert' is undefined and turn 'on' the above eslint rules
+* @todo use eslint-plugin-chai to resolve 'assert'
 */
 
 'use strict';
@@ -56,12 +56,12 @@ describe('store.js', () => {
 
     it('should get all websites from store', async () => {
       const websites = await mockStore.getAll();
-      expect(websites).to.deep.equal(mockStore._websites);
+      assert.deepEqual(websites, mockStore._websites);
     });
 
     it('should get website object for a1.com', async () => {
       const website = await mockStore.getFirstParty('a1.com');
-      expect(website).to.deep.equal(mockStore._websites['a1.com']);
+      assert.deepEqual(website, mockStore._websites['a1.com']);
     });
 
     it('error thrown when hostname is not passed for getFirstParty()',
@@ -76,7 +76,7 @@ describe('store.js', () => {
     it('should get thirdPartyRequests for a1.com', async () => {
       const thirdParties = await mockStore.getThirdParties('a1.com');
       const mockThirdParties = mockStore._websites['a1.com'].thirdPartyRequests;
-      expect(thirdParties).to.deep.equal(mockThirdParties);
+      assert.deepEqual(thirdParties, mockThirdParties);
     });
 
     it('error thrown when hostname is not passed for getThirdParties()',
@@ -90,7 +90,7 @@ describe('store.js', () => {
 
     it('should return null for getThirdParties()', async () => {
       const thirdPartyRequests = await mockStore.getThirdParties('a2.com');
-      expect(thirdPartyRequests).to.equal(null);
+      assert.equal(thirdPartyRequests, null);
     });
   });
 });
