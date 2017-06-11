f3ba4121abe6d6fca5be79842c30b19b76573549
Bianca Danforth <biancadanforth@gmail.com>
2017-06-10 19:43:59 -0700

    Integrate storage API with data capture. Fixes #42.

--- js/capture.js
+++ js/capture.js
@@ -1,5 +1,5 @@
-/* eslint no-console: "off" */
-/* eslint no-unused-vars: "off" */
+/* eslint no-unused-vars: "off"*/
+/* eslint no-undef: "off" */
 
 const capture = {
   init() {
@@ -18,31 +18,34 @@ const capture = {
   // capture third party requests
   async sendThirdParty(response) {
     const tab = await browser.tabs.get(response.tabId);
-    const tabUrl = new URL(tab.url);
+    const documentUrl = new URL(tab.url);
     const targetUrl = new URL(response.url);
     const originUrl = new URL(response.originUrl);
 
-    if (targetUrl.hostname !== tabUrl.hostname) {
-      const thirdPartyData = {
-        document: tabUrl.hostname,
+    if (targetUrl.hostname !== documentUrl.hostname) {
+      const data = {
+        document: documentUrl.hostname,
         target: targetUrl.hostname,
         origin: originUrl.hostname,
         requestTime: response.timeStamp
       };
-      console.log('storage.thirdPartyRequest:', tabUrl, thirdPartyData);
+      store.setThirdParty(
+        documentUrl.hostname,
+        targetUrl.hostname,
+        data
+      );
     }
   },
 
   // capture first party requests
   sendFirstParty(tabId, changeInfo, tab) {
-    const tabUrl = new URL(tab.url);
+    const documentUrl = new URL(tab.url);
     // ignore about:* pages and non-visible tabs
     if (tab.status === 'complete'
-      && tabUrl.protocol !== 'about:'
+      && documentUrl.protocol !== 'about:'
       && tabId !== browser.tabs.TAB_ID_NONE) {
-      const firstPartyData = { faviconUrl: tab.favIconUrl };
-      console.log('storage.firstPartyRequest:',
-        tabUrl.hostname, firstPartyData);
+      const data = { faviconUrl: tab.favIconUrl };
+      store.setFirstParty(documentUrl.hostname, data);
     }
   }
 };
