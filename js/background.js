97c8d58b38314ec37ebca75aa4c2ef270eaf53dc
Bianca Danforth <biancadanforth@gmail.com>
2017-06-12 13:58:21 -0700

    Integrate storage API with data capture. Fixes #42. (#43)
    * Integrate storage API with data capture. Fixes #42.
    
    * Integrate storage API with data capture. Fixes #42.v2

--- js/background.js
+++ js/background.js
@@ -4,8 +4,6 @@
 // When the user clicks browserAction icon in toolbar, run Lightbeam
 browser.browserAction.onClicked.addListener(runLightbeam);
 
-capture.init();
-
 async function runLightbeam() {
   // Checks to see if Lightbeam is already open.
   // Returns true if it is, false if not.
