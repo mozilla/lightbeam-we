fad35535cbd15c639efdf63f84851e70cc37f539
Bianca Danforth <biancadanforth@gmail.com>
2017-08-21 14:46:45 -0700

    Restructure lightbeam.js similar to other scripts
    Basically I just made the lightbeam.js script of the same form as capture.js, store.js, etc., where 'lightbeam' is a global object with methods. I have not modified any methods, just moved things around and renamed the anonymous function that passed an update callback to 'storeChild' as 'redraw'.

--- js/lightbeam.js
+++ js/lightbeam.js
@@ -1,61 +1,31 @@
-// transforms the object of nested objects 'websites' into a
-// usable format for d3
+const lightbeam = {
+  websites: {},
 
-/*
-  websites is expected to match this format:
-  {
-    "www.firstpartydomain.com": {
-      favicon: "http://blah...",
-      firstParty: true,
-      firstPartyHostnames: false,
-      hostname: "www.firstpartydomain.com",
-      thirdParties: [
-        "www.thirdpartydomain.com",
-        ...
-      ]
-    },
-    "www.thirdpartydomain.com": {
-      favicon: "",
-      firstParty: false,
-      firstPartyHostnames: [
-        "www.firstpartydomain.com",
-        ...
-      ],
-      hostname: "www.thirdpartydomain.com",
-      thirdParties: []
-    },
-    ...
-  }
+  async init() {
+    this.websites = await storeChild.getAll();
+    this.renderGraph();
+    this.addListeners();
+  },
 
-  nodes is expected to match this format:
-  [
-    {
-      favicon: "http://blah...",
-      firstParty: true,
-      firstPartyHostnames: false,
-      hostname: "www.firstpartydomain.com",
-      thirdParties: [
-        "www.thirdpartydomain.com",
-        ...
-      ]
-    },
-    {
-      favicon: "",
-      firstParty: false,
-      firstPartyHostnames: [
-        "www.firstpartydomain.com",
-        ...
-      ],
-      hostname: "www.thirdpartydomain.com",
-      thirdParties: []
-    },
-    ...
-  ]
+  renderGraph() {
+    const transformedData = this.transformData();
+    viz.init(transformedData.nodes, transformedData.links);
+  },
 
-  links is expected to match this format:
-  [
+  addListeners() {
+    this.downloadData();
+    this.resetData();
+    storeChild.onUpdate((data) => {
+      this.redraw(data);
+    });
+  },
+
+  // transforms the object of nested objects 'websites' into a
+  // usable format for d3
+  /*
+    websites is expected to match this format:
     {
-      source: {
+      "www.firstpartydomain.com": {
         favicon: "http://blah...",
         firstParty: true,
         firstPartyHostnames: false,
@@ -65,7 +35,7 @@
           ...
         ]
       },
-      target: {
+      "www.thirdpartydomain.com": {
         favicon: "",
         firstParty: false,
         firstPartyHostnames: [
@@ -74,97 +44,133 @@
         ],
         hostname: "www.thirdpartydomain.com",
         thirdParties: []
-      }
-    },
-    ...
-  ]
-*/
-function transformData(websites) {
-  const nodes = [];
-  let links = [];
-  for (const website in websites) {
-    const site = websites[website];
-    if (site.firstParty) {
-      const thirdPartyLinks = site.thirdParties.map((thirdParty) => {
-        return {
-          source: website,
-          target: thirdParty
-        };
-      });
-      links = links.concat(thirdPartyLinks);
+      },
+      ...
     }
-    nodes.push(websites[website]);
-  }
 
-  return {
-    nodes,
-    links
-  };
-}
+    nodes is expected to match this format:
+    [
+      {
+        favicon: "http://blah...",
+        firstParty: true,
+        firstPartyHostnames: false,
+        hostname: "www.firstpartydomain.com",
+        thirdParties: [
+          "www.thirdpartydomain.com",
+          ...
+        ]
+      },
+      {
+        favicon: "",
+        firstParty: false,
+        firstPartyHostnames: [
+          "www.firstpartydomain.com",
+          ...
+        ],
+        hostname: "www.thirdpartydomain.com",
+        thirdParties: []
+      },
+      ...
+    ]
 
-function renderGraph(websites) {
-  const transformedData = transformData(websites);
-  viz.init(transformedData.nodes, transformedData.links);
-}
-
-let websites;
-
-async function initLightBeam() {
-  websites = await storeChild.getAll();
-  renderGraph(websites);
-  addEventListeners();
-}
-
-function addEventListeners() {
-  downloadData();
-  resetData();
-}
-
-function downloadData() {
-  const saveData = document.getElementById('save-data-button');
-  saveData.addEventListener('click', async () => {
-    const data = await storeChild.getAll();
-    const blob = new Blob([JSON.stringify(data ,' ' , 2)],
-      {type : 'application/json'});
-    const url = window.URL.createObjectURL(blob);
-    const downloading = browser.downloads.download({
-      url : url,
-      filename : 'lightbeamData.json',
-      conflictAction : 'uniquify'
-    });
-    await downloading;
-  });
-}
-
-function resetData() {
-  const resetData = document.getElementById('reset-data-button');
-  resetData.addEventListener('click', async () => {
-    await storeChild.reset();
-    window.location.reload();
-  });
-}
-
-window.onload = initLightBeam;
-
-storeChild.onUpdate((data) => {
-  if (!(data.hostname in websites)) {
-    websites[data.hostname] = data;
-  }
-  if (!data.firstParty) {
-    // if we have the first parties make the link if they don't exist
-    data.firstPartyHostnames.forEach((firstPartyHostname) => {
-      if (websites[firstPartyHostname]) {
-        const firstPartyWebsite = websites[firstPartyHostname];
-        if (!('thirdParties' in firstPartyWebsite)) {
-          firstPartyWebsite.thirdParties = [];
-          firstPartyWebsite.firstParty = true;
+    links is expected to match this format:
+    [
+      {
+        source: {
+          favicon: "http://blah...",
+          firstParty: true,
+          firstPartyHostnames: false,
+          hostname: "www.firstpartydomain.com",
+          thirdParties: [
+            "www.thirdpartydomain.com",
+            ...
+          ]
+        },
+        target: {
+          favicon: "",
+          firstParty: false,
+          firstPartyHostnames: [
+            "www.firstpartydomain.com",
+            ...
+          ],
+          hostname: "www.thirdpartydomain.com",
+          thirdParties: []
         }
-        if (!(firstPartyWebsite.thirdParties.includes(data.hostname))) {
-          firstPartyWebsite.thirdParties.push(data.hostname);
-        }
+      },
+      ...
+    ]
+  */
+  transformData() {
+    const nodes = [];
+    let links = [];
+    for (const website in this.websites) {
+      const site = this.websites[website];
+      if (site.firstParty) {
+        const thirdPartyLinks = site.thirdParties.map((thirdParty) => {
+          return {
+            source: website,
+            target: thirdParty
+          };
+        });
+        links = links.concat(thirdPartyLinks);
       }
+      nodes.push(this.websites[website]);
+    }
+
+    return {
+      nodes,
+      links
+    };
+  },
+
+  downloadData() {
+    const saveData = document.getElementById('save-data-button');
+    saveData.addEventListener('click', async () => {
+      const data = await storeChild.getAll();
+      const blob = new Blob([JSON.stringify(data ,' ' , 2)],
+        {type : 'application/json'});
+      const url = window.URL.createObjectURL(blob);
+      const downloading = browser.downloads.download({
+        url : url,
+        filename : 'lightbeamData.json',
+        conflictAction : 'uniquify'
+      });
+      await downloading;
     });
-  }
-  const transformedData = transformData(websites);
-  viz.draw(transformedData.nodes, transformedData.links);
-});
+  },
+
+  resetData() {
+    const resetData = document.getElementById('reset-data-button');
+    resetData.addEventListener('click', async () => {
+      await storeChild.reset();
+      window.location.reload();
+    });
+  },
+
+  redraw(data) {
+    if (!(data.hostname in this.websites)) {
+      this.websites[data.hostname] = data;
+    }
+    if (!data.firstParty) {
+      // if we have the first parties make the link if they don't exist
+      data.firstPartyHostnames.forEach((firstPartyHostname) => {
+        if (this.websites[firstPartyHostname]) {
+          const firstPartyWebsite = this.websites[firstPartyHostname];
+          if (!('thirdParties' in firstPartyWebsite)) {
+            firstPartyWebsite.thirdParties = [];
+            firstPartyWebsite.firstParty = true;
+          }
+          if (!(firstPartyWebsite.thirdParties.includes(data.hostname))) {
+            firstPartyWebsite.thirdParties.push(data.hostname);
+          }
+        }
+      });
+    }
+    const transformedData = this.transformData(this.websites);
+    viz.draw(transformedData.nodes, transformedData.links);
+  },
+};
+
+window.onload = () => {
+  lightbeam.init();
+};
