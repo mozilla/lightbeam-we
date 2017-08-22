266aeaf85bf5cf784460751eec88389423a3f025
Princiya <princiya.marina@gmail.com>
2017-08-22 13:35:52 +0200

    graph area maximised. fixes #146

--- js/viz.js
+++ js/viz.js
@@ -7,7 +7,7 @@ const viz = {
   maxZoom: 1.5,
 
   init(nodes, links) {
-    const { width, height } = this.getDimensions('visualization');
+    const { width, height } = this.getDimensions();
     const { canvas, context } = this.createCanvas();
 
     this.canvas = canvas;
@@ -63,6 +63,11 @@ const viz = {
     return simulation;
   },
 
+  getComputedStyle(element, property) {
+    const value = window.getComputedStyle(element).getPropertyValue(property);
+    return parseInt(value);
+  },
+
   createCanvas() {
     const base = document.getElementById('visualization');
     const canvas = document.createElement('canvas');
@@ -86,8 +91,8 @@ const viz = {
     this.context.scale(this.scale, this.scale);
   },
 
-  getDimensions(id) {
-    const element = document.getElementById(id);
+  getDimensions() {
+    const element = document.body;
     const { width, height } = element.getBoundingClientRect();
 
     return {
