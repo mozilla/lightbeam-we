2053d07d7cf89a2bb15c5ed78f05bd4999f1afe6
Bianca Danforth <biancadanforth@gmail.com>
2017-08-31 16:30:13 -0700

    Add first run page. Fixes #117. (#186)
    We probably want to make a follow-up issue to check for first run with the browser extension version instead of using local storage and add a button in first-run.html to open the Lightbeam tab from the first-run page.

--- js/background.js
+++ js/background.js
@@ -1,7 +1,12 @@
 'use strict';
 
-// When the user clicks browserAction icon in toolbar, run Lightbeam
-browser.browserAction.onClicked.addListener(runLightbeam);
+async function checkFirstRun() {
+  if (await store.isFirstRun()) {
+    browser.tabs.create({ url: 'first-run.html' });
+  }
+}
+
+checkFirstRun();
 
 async function runLightbeam() {
   // Checks to see if Lightbeam is already open.
@@ -18,9 +23,12 @@ async function runLightbeam() {
   const lightbeamTab = await isOpen();
   if (!lightbeamTab) {
     // only open a new Lightbeam instance if one isn't already open.
-    browser.tabs.create({url: 'index.html'});
+    browser.tabs.create({ url: 'index.html' });
   } else if (!lightbeamTab.active) {
      // re-focus Lightbeam if it is already open but lost focus
     browser.tabs.update(lightbeamTab.id, {active: true});
   }
 }
+
+// When the user clicks browserAction icon in toolbar, run Lightbeam
+browser.browserAction.onClicked.addListener(runLightbeam);
